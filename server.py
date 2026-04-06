4ebbd1c3425946b01a2d526dc99305d461258ac4
Seldarzu <arzuuxselda@gmail.com>
2026-04-07 01:59:56 +0300

    Add programmatic port binding

--- server.py
+++ server.py
@@ -62,3 +62,10 @@ def explore_radius(req: ExploreRadiusRequest):
     places = final_df.to_dict(orient="records")
     
     return {"places": places}
+
+if __name__ == "__main__":
+    import uvicorn
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("server:app", host="0.0.0.0", port=port)
+
